1fec1e249c5f24e6cb94f7138fcbe2a1f0a58033
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-23 08:01:33 -0300

    first commit

---
+++ src/main/java/com/myproject/poc_structured_concurrency_with_resilience4J/PocStructuredConcurrencyWithResilience4JApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_structured_concurrency_with_resilience4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocStructuredConcurrencyWithResilience4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocStructuredConcurrencyWithResilience4JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/poc_structured_concurrency_with_resilience4J/PocStructuredConcurrencyWithResilience4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_structured_concurrency_with_resilience4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocStructuredConcurrencyWithResilience4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
